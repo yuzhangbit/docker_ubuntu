86d38c9d7f7fbdeccafe4bdaf5f2b84d97146015
yuzhangbit <yu.zhang.bit@gmail.com>
2018-12-02 23:33:38 +0800

    enable user sudo without password

---
+++ ubuntu/build_local_xenial_robot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e # exit on first error
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASENAME=$(basename "$CURRENT_DIR")
+
+
+cd ${CURRENT_DIR} && docker build -t local_xenial_robot ./ -f Dockerfile.local 
