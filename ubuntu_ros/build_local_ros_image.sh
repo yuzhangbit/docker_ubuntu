879bcdeb3be53de68102743490c0d77f688b3aa6
yuzhangbit <yu.zhang.bit@gmail.com>
2018-12-13 11:23:44 +0800

    add users

--- ubuntu_ros/build_local_ros_image.sh
+++ ubuntu_ros/build_local_ros_image.sh
@@ -5,4 +5,4 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASENAME=$(basename "$CURRENT_DIR")
 
 
-cd ${CURRENT_DIR} && docker build -t local_xenial_ros ./ -f Dockerfile.local 
+cd ${CURRENT_DIR} && docker build -t local_xenial_ros_robot ./ -f Dockerfile.local 
